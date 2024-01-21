26e11e66587db020dd62fa3ed04e9ef2fb28aeb3
Eduard-Prinz <144338386+Eduard-Prinz@users.noreply.github.com>
2024-01-21 12:24:21 +0300

    Начало выполнения ДЗ №25

---
+++ src/main/java/hw24/hw/Hw2525Application.java
@@ -0,0 +1,13 @@
+package hw24.hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw2525Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw2525Application.class, args);
+	}
+
+}
---
+++ src/test/java/hw24/hw/Hw2525ApplicationTests.java
@@ -0,0 +1,13 @@
+package hw24.hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw2525ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
